9261bfa3a7d1fba238bf8fd4dba323bc80d866a6
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-03 06:03:25 +0100

    feat: handle questionnaires and bundles

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import example_2 from '../examples/example_2.json';
 import example_3 from '../examples/example_3.json';
 import example_4 from '../examples/example_4.json';
 import example_5 from '../examples/example_5.json';
-// import example_6 from '../examples/example_6.json';
+import example_6 from '../examples/example_6.json';
 import example_7 from '../examples/example_7.json';
 
 const examples: Record<any, any> = [
@@ -12,7 +12,7 @@ const examples: Record<any, any> = [
   example_3,
   example_4,
   example_5,
-  // example_6,
+  example_6,
   example_7,
 ];
 
@@ -28,7 +28,25 @@ const flattenQuestionnaire = (obj: Record<PropertyKey, any>) => {
   });
 };
 
-examples.forEach((example) => {
-  console.log('************************');
-  example.item.filter((i) => flattenQuestionnaire(i));
-});
+const handleQuestionnaires = (questionnaire: Record<PropertyKey, any>) => {
+  console.log('*'.repeat(50));
+  questionnaire.item.filter((i) => flattenQuestionnaire(i));
+};
+
+const handleBundle = (bundle: Record<PropertyKey, any>) => {
+  const entries = bundle.entry || [];
+  const resources = entries.map((entry) => entry.resource);
+  resources.forEach((resource) => handleResource(resource));
+};
+
+const handleResource = (resource: Record<PropertyKey, any>) => {
+  if (resource.resourceType === 'Questionnaire') {
+    handleQuestionnaires(resource);
+  } else if (resource.resourceType === 'Bundle') {
+    handleBundle(resource);
+  } else {
+    console.warn(`resourceType: ${resource.resourceType} not handled`);
+  }
+};
+
+examples.forEach((example) => handleResource(example));

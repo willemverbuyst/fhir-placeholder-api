9398d6a61cf0ea0cebfdf81779f765da89eb8dcb
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-03 17:06:01 +0100

    chore: make examples ts objects

---
+++ src/examples/index.ts
@@ -0,0 +1,7 @@
+export * from './example_1';
+export * from './example_2';
+export * from './example_3';
+export * from './example_4';
+export * from './example_5';
+export * from './example_6';
+export * from './example_7';
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import example_1 from '../examples/example_1.json';
-import example_2 from '../examples/example_2.json';
-import example_3 from '../examples/example_3.json';
-import example_4 from '../examples/example_4.json';
-import example_5 from '../examples/example_5.json';
-import example_6 from '../examples/example_6.json';
-import example_7 from '../examples/example_7.json';
+import {
+  example_1,
+  example_2,
+  example_3,
+  example_4,
+  example_5,
+  example_6,
+  example_7,
+} from './examples';
+import { Bundle } from './interfaces/bundle';
+import { ValueSet } from './interfaces/general';
+import { Item, Questionnaire } from './interfaces/questionnaire';
 
-const examples: Record<any, any> = [
+const examples: Array<Questionnaire | Bundle> = [
   example_1,
   example_2,
   example_3,
@@ -16,10 +21,7 @@ const examples: Record<any, any> = [
   example_7,
 ];
 
-const flattenQuestionnaire = (
-  obj: Record<PropertyKey, any>,
-  flatQ: Record<PropertyKey, any>
-) => {
+const flattenQuestionnaire = (obj: Item, flatQ: Record<PropertyKey, any>) => {
   Object.keys(obj).forEach((prop) => {
     if (typeof obj[prop] === 'object') {
       flattenQuestionnaire(obj[prop], flatQ);
@@ -32,23 +34,20 @@ const flattenQuestionnaire = (
 };
 
 const handleQuestionnaires = (
-  questionnaire: Record<PropertyKey, any>,
+  questionnaire: Questionnaire,
   flatQ: Record<PropertyKey, any>
 ) => {
   questionnaire.item.map((i) => flattenQuestionnaire(i, flatQ));
 };
 
-const handleBundle = (
-  bundle: Record<PropertyKey, any>,
-  flatQ: Record<PropertyKey, any>
-) => {
+const handleBundle = (bundle: Bundle, flatQ: Record<PropertyKey, any>) => {
   const entries = bundle.entry || [];
   const resources = entries.map((entry) => entry.resource);
   resources.forEach((resource) => handleResource(resource, flatQ));
 };
 
 const handleResource = (
-  resource: Record<PropertyKey, any>,
+  resource: Questionnaire | Bundle | ValueSet,
   flatQ: Record<PropertyKey, any>
 ) => {
   if (resource.resourceType === 'Questionnaire') {
@@ -56,7 +55,7 @@ const handleResource = (
   } else if (resource.resourceType === 'Bundle') {
     handleBundle(resource, flatQ);
   } else {
-    console.warn(`resourceType: ${resource.resourceType} not handled`);
+    console.warn('Resource could not be processed');
   }
 };
 

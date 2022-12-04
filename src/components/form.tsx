b8b3b49dde2c474eecb9c1b69c60e98e7126549b
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-04 05:38:15 +0100

    feat: add checkbox

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import ExampleContext from '../store';
+import { Checkbox } from './checkbox';
 import { Text } from './text';
 
 export const Form = () => {
@@ -13,17 +14,21 @@ export const Form = () => {
           {/* <pre>{JSON.stringify(questionnaire, null, 4)}</pre> */}
           <Col>
             {questionnaire
-              ? Object.values(questionnaire).map((q, idx) => (
-                  <Text
-                    key={q?.linkId || idx}
-                    text={
-                      q?.text ||
-                      (q?.code && q.code[0].display) ||
-                      (q?.code && q.code[0].code) ||
-                      'nothing to show'
-                    }
-                  />
-                ))
+              ? Object.values(questionnaire).map((q, idx) =>
+                  q?.type === 'boolean' ? (
+                    <Checkbox item={q} />
+                  ) : (
+                    <Text
+                      key={q?.linkId || idx}
+                      text={
+                        q?.text ||
+                        (q?.code && q.code[0].display) ||
+                        (q?.code && q.code[0].code) ||
+                        ''
+                      }
+                    />
+                  )
+                )
               : null}
           </Col>
         </Row>

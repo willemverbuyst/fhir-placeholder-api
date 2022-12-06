cb1b518aa93825ad72a01dd0f50fc44ad33bbdf8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-06 04:53:17 +0100

    feat: add prefix

--- src/business/label.ts
+++ src/business/label.ts
@@ -1,7 +1,11 @@
 import { Item } from '../interfaces/questionnaire';
 
-export const getLabel = (item: Item) =>
-  item?.text ||
-  (item?.code && item.code[0].display) ||
-  (item?.code && item.code[0].code) ||
-  '';
+export const getLabel = (item: Item) => {
+  const prefix = item?.prefix;
+  const text =
+    item?.text ||
+    (item?.code && item.code[0].display) ||
+    (item?.code && item.code[0].code) ||
+    '';
+  return [prefix, text].join(' ').trim();
+};

5786ed9b7785a7dfb8278bb89588762d799b0aae
Willem Verbuyst <willemverbuyst@gmail.com>
2022-12-09 19:43:59 +0100

    feat: add buttons

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import ExampleContext from '../../store';
 import { Title } from '../title';
@@ -6,8 +6,21 @@ import { Title } from '../title';
 import { QuestionCard } from './questionCard';
 
 export const Form = () => {
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const questionnaire = useContext(ExampleContext);
   const questionnaireItems = questionnaire?.items;
+
+  const handleNext = () => {
+    if (currentQuestion < questionnaire.items.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  };
 
   return (
     <ExampleContext.Provider value={questionnaire}>
@@ -15,11 +28,15 @@ export const Form = () => {
         {questionnaire && <Title questionnaire={questionnaire} />}
         <Row>
           <Col>
-            {questionnaireItems
-              ? Object.values(questionnaireItems).map((item, idx) => (
-                  <QuestionCard key={idx} item={item} />
-                ))
-              : null}
+            {questionnaireItems ? (
+              <QuestionCard
+                item={Object.values(questionnaireItems)[currentQuestion]}
+                onNext={handleNext}
+                onPrevious={handlePrevious}
+                displayNext={currentQuestion < questionnaire.items.length - 1}
+                displayPrevious={currentQuestion > 0}
+              />
+            ) : null}
           </Col>
         </Row>
       </Container>
--- src/components/form/questionCard.tsx
+++ src/components/form/questionCard.tsx
@@ -1,15 +1,40 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Stack } from 'react-bootstrap';
 import { InputSwitch } from './inputSwitch';
 
-export const QuestionCard = ({ item }: any) => {
+export const QuestionCard = ({
+  item,
+  onNext,
+  onPrevious,
+  displayNext,
+  displayPrevious,
+}: any) => {
   return (
     <div>
       <Card>
         <Card.Body>
           <InputSwitch item={item} />
-          <Button variant="primary" type="submit">
-            Continue
-          </Button>
+          <Stack direction="horizontal">
+            {displayPrevious ? (
+              <Button
+                className="m-2"
+                variant="outline-primary"
+                type="submit"
+                onClick={onPrevious}
+              >
+                Previous
+              </Button>
+            ) : null}
+            {displayNext ? (
+              <Button
+                className="m-2"
+                variant="primary"
+                type="submit"
+                onClick={onNext}
+              >
+                Next
+              </Button>
+            ) : null}
+          </Stack>
         </Card.Body>
       </Card>
     </div>

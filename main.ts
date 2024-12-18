4979a60d2f9b74de9a0095cadc6032f82e3c1171
Willem Verbuyst <willemverbuyst@gmail.com>
2024-12-18 05:11:30 +0100

    Use lowdb for data storage

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 // @ts-types="npm:@types/express@4.17.15"
 import express from "npm:express@4.18.2";
+import { JSONFilePreset } from "npm:lowdb/node";
+
+type Data = {
+  names: string[];
+};
+
+const defaultData: Data = { names: [] };
+const db = await JSONFilePreset<Data>("db.json", defaultData);
+
+db.data.names.push("foo");
+db.data.names.push("bar");
+db.data.names.push("quux");
 
 const app = express();
 
@@ -7,8 +19,9 @@ app.get("/", (_, res) => {
   res.send("Hello world!");
 });
 
-app.get("/", (_, res) => {
-  res.send("Hello world!");
+app.get("/patients", (_, res) => {
+  db.read();
+  res.send(db.data.names);
 });
 
 app.listen(8000);

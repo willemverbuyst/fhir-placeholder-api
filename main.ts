4bbab4c19379caa64f231ebb18619abc038244c6
Willem Verbuyst <willemverbuyst@gmail.com>
2024-12-18 18:01:18 +0100

    Add routes and update response objects

--- main.ts
+++ main.ts
@@ -19,27 +19,64 @@ const app = express();
 app.use(morganMiddleware);
 
 app.get("/", (_, res) => {
-  res.send("Hello world!");
+  res.send("Fhir Placeholder Api");
 });
 
 app.get("/patients", (_, res) => {
   const patients = db.data.patients;
-  res.send(patients);
+
+  res.send({ status: "success", lengtt: patients.length, data: patients });
 });
 
 app.get("/patients/:id", (req, res) => {
   const patient = db.data.patients.find((p) => p.id === req.params.id);
-  res.send(patient);
+
+  if (!patient) {
+    res
+      .status(404)
+      .send({ status: "fail", data: null, message: "patient not found" });
+  } else {
+    res.send({ status: "success", data: patient });
+  }
 });
 
 app.get("/episodes", (_, res) => {
   const episodes = db.data.episodes;
-  res.send(episodes);
+
+  res.send({ status: "success", lengtt: episodes.length, data: episodes });
+});
+
+app.get("/episodes/:id", (req, res) => {
+  const episode = db.data.episodes.find((e) => e.id === req.params.id);
+  if (!episode) {
+    res.status(404).send({
+      status: "fail",
+      data: null,
+      message: "episode of care not found",
+    });
+  } else {
+    res.send({ status: "success", data: episode });
+  }
 });
 
 app.get("/conditions", (_, res) => {
   const conditions = db.data.conditions;
-  res.send(conditions);
+
+  res.send({ status: "success", lengtt: conditions.length, data: conditions });
+});
+
+app.get("/conditions/:id", (req, res) => {
+  const condition = db.data.conditions.find((c) => c.id === req.params.id);
+
+  if (!condition) {
+    res.status(404).send({
+      status: "fail",
+      data: null,
+      message: "condition not found",
+    });
+  } else {
+    res.send({ status: "success", data: condition });
+  }
 });
 
 app.listen(8000, () => {

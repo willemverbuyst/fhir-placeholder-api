d28ed37f37e9b1ec0d469382da1910b1f02e2ba6
Willem Verbuyst <willemverbuyst@gmail.com>
2025-03-29 07:00:58 +0100

    Test ConditionService

---
+++ app/services/condition-service.test.ts
@@ -0,0 +1,46 @@
+import {
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.151.0/testing/bdd.ts";
+import { assertEquals } from "jsr:@std/assert";
+import { ConditionService } from "./condition-service.ts";
+import { testDataStore } from "./test/dataStore.ts";
+
+describe("ConditionService", () => {
+  let conditionService: ConditionService;
+
+  beforeEach(() => {
+    conditionService = new ConditionService(testDataStore);
+  });
+
+  it("should return all conditions", () => {
+    const conditions = conditionService.getAll();
+    assertEquals(conditions, testDataStore.conditions);
+  });
+
+  it("should return a condition by ID", () => {
+    const conditionId = testDataStore.conditions[0].id;
+    const condition = conditionService.getById(conditionId);
+    assertEquals(condition, testDataStore.conditions[0]);
+  });
+
+  it("should return undefined if condition ID does not exist", () => {
+    const condition = conditionService.getById("nonexistent-id");
+    assertEquals(condition, undefined);
+  });
+
+  it("should return conditions by patient ID", () => {
+    const patientId = "1";
+    const conditions = conditionService.getByPatientId(patientId);
+    const expectedConditions = [testDataStore.conditions[0]];
+    assertEquals(conditions, expectedConditions);
+  });
+
+  it("should return an empty array if no conditions match the patient ID", () => {
+    const conditions = conditionService.getByPatientId(
+      "nonexistent-patient-id"
+    );
+    assertEquals(conditions, []);
+  });
+});
---
+++ app/services/test/dataStore.ts
@@ -0,0 +1,42 @@
+import { Data } from "../../models/data.ts";
+
+export const testDataStore: Data = {
+  patients: [],
+  episodes: [],
+  organizations: [],
+  practitioners: [],
+  conditions: [
+    {
+      id: "1",
+      note: [{ text: "test note" }],
+      resourceType: "Condition",
+      subject: {
+        reference: "Patient/1",
+      },
+      clinicalStatus: {
+        coding: [
+          {
+            code: "active",
+            system: "http://terminology.hl7.org/CodeSystem/condition-clinical",
+          },
+        ],
+      },
+    },
+    {
+      id: "2",
+      note: [{ text: "test note" }],
+      resourceType: "Condition",
+      subject: {
+        reference: "Patient/2",
+      },
+      clinicalStatus: {
+        coding: [
+          {
+            code: "active",
+            system: "http://terminology.hl7.org/CodeSystem/condition-clinical",
+          },
+        ],
+      },
+    },
+  ],
+};

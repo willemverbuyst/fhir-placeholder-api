27deb4a872ccdcb115bd32fa79e5b292b6610775
Willem Verbuyst <willemverbuyst@gmail.com>
2025-03-31 04:50:56 +0200

    Set up config module for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middlewares';
 import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [PatientsModule],
+  imports: [
+    PatientsModule,
+    ConfigModule.forRoot({
+      // to use ConfigModule in other modules set isGlobal to true
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

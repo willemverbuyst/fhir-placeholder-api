1eeb52f7f954d15e1eb26c73ec2bc8ad4dbd7435
Willem Verbuyst <willemverbuyst@gmail.com>
2025-03-31 04:21:36 +0200

    Set global prefix

---
+++ src/config/version.ts
@@ -0,0 +1,1 @@
+export const FHIR_VERSION_R5 = 'R5';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FHIR_VERSION_R5 } from './config/version';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v2/' + FHIR_VERSION_R5);
+
   await app.listen(3000);
 }
 bootstrap();

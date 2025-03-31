ca07cb82f3d9929607457be42418fdd0d9370e00
Willem Verbuyst <willemverbuyst@gmail.com>
2025-03-31 04:17:37 +0200

    Create crud controller for patients with nestjs cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [],
+  imports: [PatientsModule],
   controllers: [AppController],
   providers: [AppService],
 })

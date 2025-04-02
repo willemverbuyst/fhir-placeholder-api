0a0f16382c613793d22b87cba13a4af020c90fd7
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-02 08:00:33 +0200

    Test patient service findAll and findOne

--- src/patients/patients.service.spec.ts
+++ src/patients/patients.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataStore } from '../db/dataStore.service';
+import { testDataStore } from '../test/testDataStore';
 import { PatientsService } from './patients.service';
 
 describe('PatientsService', () => {
@@ -7,7 +8,10 @@ describe('PatientsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientsService, DataStore],
+      providers: [
+        PatientsService,
+        { provide: DataStore, useValue: testDataStore },
+      ],
     }).compile();
 
     service = module.get<PatientsService>(PatientsService);
@@ -15,5 +19,18 @@ describe('PatientsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all patients', () => {
+    const patients = service.findAll();
+    expect(patients).toBeDefined();
+    expect(patients.length).toBe(2);
+  });
+
+  it('should return an patient by id', () => {
+    const patient = service.findOne('1');
+    expect(patient).toBeDefined();
+    expect(patient!.id).toBe('1');
+    expect(patient!.resourceType).toBe('Patient');
   });
 });

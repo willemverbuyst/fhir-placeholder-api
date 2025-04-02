5d54157eb132f9ea0a5e2284e5e1a9198ba5ddf3
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-02 08:39:07 +0200

    Test organizations controller

--- src/organizations/organizations.controller.spec.ts
+++ src/organizations/organizations.controller.spec.ts
@@ -1,21 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataStore } from '../db/dataStore.service';
+import { testDataStore } from '../test/testDataStore';
 import { OrganizationsController } from './organizations.controller';
 import { OrganizationsService } from './organizations.service';
 
 describe('OrganizationsController', () => {
   let controller: OrganizationsController;
+  let service: OrganizationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationsController],
-      providers: [OrganizationsService, DataStore],
+      providers: [
+        {
+          provide: OrganizationsService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        { provide: DataStore, useValue: testDataStore },
+      ],
     }).compile();
 
     controller = module.get<OrganizationsController>(OrganizationsController);
+    service = module.get<OrganizationsService>(OrganizationsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findAll method of OrganizationsService', () => {
+    controller.findAll();
+
+    expect(service.findAll).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call findOne method of OrganizationsService', () => {
+    const id = '1';
+    controller.findOne(id);
+
+    expect(service.findOne).toHaveBeenCalledWith(id);
   });
 });

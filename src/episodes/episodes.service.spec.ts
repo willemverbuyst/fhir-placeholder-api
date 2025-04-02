19f201aea96524e66ced79207d124e6486a3f6cf
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-02 08:09:00 +0200

    Test episode service

--- src/episodes/episodes.service.spec.ts
+++ src/episodes/episodes.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataStore } from '../db/dataStore.service';
+import { testDataStore } from '../test/testDataStore';
 import { EpisodesService } from './episodes.service';
 
 describe('EpisodesService', () => {
@@ -7,7 +8,10 @@ describe('EpisodesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EpisodesService, DataStore],
+      providers: [
+        EpisodesService,
+        { provide: DataStore, useValue: testDataStore },
+      ],
     }).compile();
 
     service = module.get<EpisodesService>(EpisodesService);
@@ -15,5 +19,36 @@ describe('EpisodesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all episodes', () => {
+    const episodes = service.findAll();
+    expect(episodes).toBeDefined();
+    expect(episodes.length).toBe(2);
+  });
+
+  it('should return an episode by id', () => {
+    const episode = service.findOne('1');
+    expect(episode).toBeDefined();
+    expect(episode!.id).toBe('1');
+    expect(episode!.resourceType).toBe('EpisodeOfCare');
+  });
+
+  it("should return undefined for an episode that doesn't exist", () => {
+    const episode = service.findOne('999');
+    expect(episode).toBeUndefined();
+  });
+
+  it('should return episodes by patient id', () => {
+    const episodes = service.findByPatientId('1');
+    expect(episodes).toBeDefined();
+    expect(episodes.length).toBe(1);
+    expect(episodes[0].patient.reference).toBe('Patient/1');
+  });
+
+  it("should return an empty array for a patient id that doesn't exist", () => {
+    const episodes = service.findByPatientId('999');
+    expect(episodes).toBeDefined();
+    expect(episodes.length).toBe(0);
   });
 });

2670e8dd9882533d21e1163df277776fa0cb3831
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-02 08:05:48 +0200

    Test condition service

--- src/conditions/conditions.service.spec.ts
+++ src/conditions/conditions.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataStore } from '../db/dataStore.service';
+import { testDataStore } from '../test/testDataStore';
 import { ConditionsService } from './conditions.service';
 
 describe('ConditionsService', () => {
@@ -7,7 +8,10 @@ describe('ConditionsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConditionsService, DataStore],
+      providers: [
+        ConditionsService,
+        { provide: DataStore, useValue: testDataStore },
+      ],
     }).compile();
 
     service = module.get<ConditionsService>(ConditionsService);
@@ -15,5 +19,23 @@ describe('ConditionsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all conditions', () => {
+    const conditions = service.findAll();
+    expect(conditions).toBeDefined();
+    expect(conditions.length).toBe(2);
+  });
+
+  it('should return an condition by id', () => {
+    const condition = service.findOne('1');
+    expect(condition).toBeDefined();
+    expect(condition!.id).toBe('1');
+    expect(condition!.resourceType).toBe('Condition');
+  });
+
+  it("should return undefined for an condition that doesn't exist", () => {
+    const condition = service.findOne('999');
+    expect(condition).toBeUndefined();
   });
 });

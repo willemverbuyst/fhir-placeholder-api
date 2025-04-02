e23eb07a9260ae080f422b680f95f872cbd00010
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-02 08:37:43 +0200

    Test patients controller

--- src/patients/patients.controller.spec.ts
+++ src/patients/patients.controller.spec.ts
@@ -1,21 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataStore } from '../db/dataStore.service';
+import { testDataStore } from '../test/testDataStore';
 import { PatientsController } from './patients.controller';
 import { PatientsService } from './patients.service';
 
 describe('PatientsController', () => {
   let controller: PatientsController;
+  let service: PatientsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientsController],
-      providers: [PatientsService, DataStore],
+      providers: [
+        {
+          provide: PatientsService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        { provide: DataStore, useValue: testDataStore },
+      ],
     }).compile();
 
     controller = module.get<PatientsController>(PatientsController);
+    service = module.get<PatientsService>(PatientsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findAll method of PatientsService', () => {
+    controller.findAll();
+
+    expect(service.findAll).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call findOne method of PatientsService', () => {
+    const id = '1';
+    controller.findOne(id);
+
+    expect(service.findOne).toHaveBeenCalledWith(id);
   });
 });

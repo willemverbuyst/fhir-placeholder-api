2461ffb2ad43c2162150b348ea61c63fb0f01fe5
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-04 08:35:01 +0200

    Add swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,6 +17,15 @@ async function bootstrap() {
 
   // whitelist: true will strip properties that are not in the DTO
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Fhir R5 API')
+    .setDescription('Server with dummy data for FHIR R5')
+    .setVersion('2.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const port = app.get(ConfigService).get('PORT') || 3000;
   await app.listen(port, () => {

24e0c58868d18d4eec6e1ad9a3a9a99b516d5c2e
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-05 05:07:39 +0200

    Update tests for patient service

--- src/patients/patients.service.ts
+++ src/patients/patients.service.ts
@@ -5,29 +5,17 @@ import { DataStore } from '../db/dataStore.service';
 export class PatientsService {
   constructor(private readonly repo: DataStore) {}
 
-  create() {
-    return 'This action adds a new patient';
-  }
-
-  findAll() {
+  async findAll() {
     return this.repo.patients;
   }
 
-  findOne(id: string) {
+  async findOne(id: string) {
     return this.repo.patients.find((patient) => patient.id === id);
   }
 
-  findAllEpisodesForPatient(id: string) {
+  async findAllEpisodesForPatient(id: string) {
     return this.repo.episodes.filter(
       (episode) => episode.patient.reference?.split('/')[1] === id,
     );
-  }
-
-  update(id: number) {
-    return `This action updates a #${id} patient`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} patient`;
   }
 }

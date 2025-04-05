02a0d252297a12567b410bccc50a03e68fb01179
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-05 05:12:55 +0200

    Update tests for patient controller

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 
 @Controller('patients')
@@ -6,17 +6,27 @@ export class PatientsController {
   constructor(private readonly patientsService: PatientsService) {}
 
   @Get()
-  findAll() {
-    return this.patientsService.findAll();
+  async findAll() {
+    const patients = await this.patientsService.findAll();
+
+    return patients;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.patientsService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const patient = await this.patientsService.findOne(id);
+
+    if (!patient) {
+      throw new NotFoundException('patient not found');
+    }
+
+    return patient;
   }
 
   @Get(':id/episodes')
-  findAllEpisodesForPatient(@Param('id') id: string) {
-    return this.patientsService.findAllEpisodesForPatient(id);
+  async findAllEpisodesForPatient(@Param('id') id: string) {
+    const episodes = await this.patientsService.findAllEpisodesForPatient(id);
+
+    return episodes;
   }
 }

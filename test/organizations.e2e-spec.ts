a4769294a70afe59239dfbb85a9cc896230422a7
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-08 03:59:46 +0200

    Update e2e for finding organizations by name

--- test/organizations.e2e-spec.ts
+++ test/organizations.e2e-spec.ts
@@ -31,6 +31,19 @@ describe('OrganizationsController (e2e)', () => {
       });
   });
 
+  it('/organizations?name=test%20organization (GET) - OK', async () => {
+    return request(app.getHttpServer())
+      .get('/organizations?name=test%20organization')
+      .expect(200)
+      .then((res) => {
+        const organizations = res.body;
+        expect(organizations).toBeDefined();
+        expect(organizations).toHaveLength(2);
+        expect(organizations[0].name).toBe('test organization');
+        expect(organizations[0].name).toBe('test organization');
+      });
+  });
+
   it('/organizations/:id (GET) - OK', async () => {
     return request(app.getHttpServer())
       .get('/organizations/1')

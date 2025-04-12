c167f0638d52e2991224ffcf892f771ef00e9e6e
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-12 04:17:49 +0200

    Create dummy data for observations

---
+++ src/db/resources/observation.ts
@@ -0,0 +1,38 @@
+import { Observation } from 'fhir/r5';
+import { v4 as uuidV4 } from 'uuid';
+import { getRandomElement } from '../helpers/getRandomElement';
+import { observationCodes } from '../valueSets/observation-code-value-set';
+
+export enum ObservationStatus {
+  REGISTERED = 'registered',
+  PRELIMINARY = 'preliminary',
+  FINAL = 'final',
+  AMENDED = 'amended',
+  CORRECTED = 'corrected',
+  CANCELLED = 'cancelled',
+  ENTERED_IN_ERROR = 'entered-in-error',
+  UNKNOWN = 'unknown',
+}
+
+export function createObservation(
+  patientId: string,
+  encounterId: string,
+): Observation {
+  return {
+    id: uuidV4(),
+    resourceType: 'Observation',
+    status: getRandomElement([
+      ObservationStatus.AMENDED,
+      ObservationStatus.CANCELLED,
+      ObservationStatus.CORRECTED,
+      ObservationStatus.ENTERED_IN_ERROR,
+      ObservationStatus.FINAL,
+      ObservationStatus.PRELIMINARY,
+      ObservationStatus.REGISTERED,
+      ObservationStatus.UNKNOWN,
+    ]),
+    code: { coding: [getRandomElement(observationCodes)] },
+    encounter: { reference: `Encounter/${encounterId}` },
+    subject: { reference: `Patient/${patientId}` },
+  };
+}

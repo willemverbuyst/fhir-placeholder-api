edb142ccb926777967fafd8a818e3a7634414389
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-14 03:34:51 +0200

    Handle encounter search params

--- src/Encounter/encounter.controller.ts
+++ src/Encounter/encounter.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
+import { ApiOkResponse, ApiQuery } from '@nestjs/swagger';
 import { Bundle, Encounter } from 'fhir/r5';
 import { Id } from 'src/types';
+import { GetEncounterDto } from './dto/get-encounter.dto';
 import { EncounterService } from './encounter.service';
 import { encounterBundleExample } from './examples/encounter-bundle.example';
 
@@ -13,8 +14,29 @@ export class EncounterController {
     description: 'All encounters',
     example: encounterBundleExample,
   })
+  @ApiQuery({
+    name: 'patient',
+    required: false,
+    description: 'Filter encounters by subject',
+    type: String,
+  })
+  @ApiQuery({
+    name: 'episode=of-care',
+    required: false,
+    description: 'Filter encounters by episode of care',
+    type: String,
+  })
   @Get()
-  async findAll(): Promise<Bundle<Encounter & Id>> {
-    return await this.encounterService.findAll();
+  async findAll(
+    @Query(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    )
+    query?: GetEncounterDto,
+  ): Promise<Bundle<Encounter & Id>> {
+    return await this.encounterService.findAll(query);
   }
 }

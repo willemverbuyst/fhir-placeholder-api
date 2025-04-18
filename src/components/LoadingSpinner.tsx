780f45c074818b1aaa567e07d9eb3a8260ee6500
Willem Verbuyst <willemverbuyst@gmail.com>
2025-04-18 04:11:21 +0200

    Use tailwind for styling

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,5 +1,11 @@
 import "./loadingSpinner.css";
 
 export function LoadingSpinner() {
-  return <span className="loader" />;
+  return (
+    <div className="h-screen flex ">
+      <div className="m-auto">
+        <span className="loader" />
+      </div>
+    </div>
+  );
 }
--- src/resources/organizations.resource.tsx
+++ src/resources/organizations.resource.tsx
@@ -12,10 +12,17 @@ export function Organizations() {
   if (error) return "An error has occurred: " + error.message;
 
   return (
-    <ul>
-      {data.entry?.map(({ resource }) => (
-        <li key={resource?.id}>{resource?.id}</li>
-      ))}
-    </ul>
+    <section>
+      <ul className="flex justify-between">
+        {data.entry?.map(({ resource }) => (
+          <li
+            key={resource?.id}
+            className="bg-amber-600 p-5 rounded-md text-white"
+          >
+            {resource?.id}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }

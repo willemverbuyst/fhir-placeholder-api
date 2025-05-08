158c85fe0b67107295ea5676b3b217a56423d800
Willem Verbuyst <willemverbuyst@gmail.com>
2025-05-08 04:41:55 +0200

    Fix errors introduced with update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -41,6 +41,6 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes({ path: "*", method: RequestMethod.ALL });
+      .forRoutes({ path: "*path", method: RequestMethod.ALL });
   }
 }

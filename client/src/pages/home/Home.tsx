15591e93fa0dabc505c9e1be035efa57cde67498
Willem Verbuyst <willemverbuyst@gmail.com>
2025-05-11 06:02:33 +0200

    Fetch metadata for homepage

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { createResourcesQueryOptions } from "../../query/resources.query";
+
 export function Home() {
-  return <h1>Home</h1>;
+  const { isPending, error, data } = useQuery(
+    createResourcesQueryOptions({ url: "metadata" }),
+  );
+
+  if (isPending) return <p>...loading</p>;
+
+  if (error) return <p>...error</p>;
+
+  return (
+    <section className="w-[50vw] m-auto">
+      <code>{JSON.stringify(data)}</code>
+    </section>
+  );
 }

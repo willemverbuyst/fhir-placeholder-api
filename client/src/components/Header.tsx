49366bcccdc6a2e69c8551695f2ffa0468d4e86f
Willem Verbuyst <willemverbuyst@gmail.com>
2025-05-12 07:41:43 +0200

    Work on layout header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,39 +2,34 @@ import { NavLink } from "react-router";
 
 export function Header() {
   return (
-    <nav className="flex gap-4 p-3">
-      <NavLink
-        to="/"
-        style={({ isActive }) => ({
-          color: isActive ? "red" : "black",
-        })}
-      >
-        Capability Statement
-      </NavLink>
-      <NavLink
-        to="/graph"
-        style={({ isActive }) => ({
-          color: isActive ? "red" : "black",
-        })}
-      >
-        Graph
-      </NavLink>
-      <NavLink
-        to="/search-sort-filter"
-        style={({ isActive }) => ({
-          color: isActive ? "red" : "black",
-        })}
-      >
-        Search Sort Filter
-      </NavLink>
-      <NavLink
-        to="/create"
-        style={({ isActive }) => ({
-          color: isActive ? "red" : "black",
-        })}
-      >
-        Create
-      </NavLink>
-    </nav>
+    <section className="flex flex-col items-center my-6">
+      <h1 className="text-3xl">Fhir Placeholder Api</h1>
+      <nav className="flex gap-4 p-3">
+        <NavLink
+          to="/"
+          className="aria-[current=page]:text-blue-500 hover:underline"
+        >
+          Capability Statement
+        </NavLink>
+        <NavLink
+          to="/graph"
+          className="aria-[current=page]:text-blue-500 hover:underline"
+        >
+          Graph
+        </NavLink>
+        <NavLink
+          to="/search-sort-filter"
+          className="aria-[current=page]:text-blue-500 hover:underline"
+        >
+          Search Sort Filter
+        </NavLink>
+        <NavLink
+          to="/create"
+          className="aria-[current=page]:text-blue-500 hover:underline"
+        >
+          Create
+        </NavLink>
+      </nav>
+    </section>
   );
 }

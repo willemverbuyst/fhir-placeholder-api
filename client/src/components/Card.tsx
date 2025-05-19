72603b5c6ade7efea009a4e02947a14b229747d2
Willem Verbuyst <willemverbuyst@gmail.com>
2025-05-19 08:16:11 +0200

    Have fixed width for cards

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -10,7 +10,7 @@ export function Card({
   return (
     <section
       className={cn(
-        "flex flex-col gap-2 text-white p-4 rounded-md",
+        "flex flex-col gap-2 text-white p-4 rounded-md w-[600px]",
         bgColor ?? "bg-white text-black border-2",
       )}
     >

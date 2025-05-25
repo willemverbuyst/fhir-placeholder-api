b9a9d5c5206e0c29e4663869e5491272071212c9
Willem Verbuyst <willemverbuyst@gmail.com>
2025-05-25 09:39:32 +0200

    Use fixed bg-color for ListItem

--- client/src/pages/graph/ListItem.tsx
+++ client/src/pages/graph/ListItem.tsx
@@ -2,11 +2,9 @@ import { type JSX, useState } from "react";
 
 export function ListItem({
   id,
-  className,
   children,
 }: {
   id: string;
-  className: string;
   children?: JSX.Element;
 }) {
   const [zoomIn, setZoomIn] = useState<string | undefined>();
@@ -15,7 +13,9 @@ export function ListItem({
     <section className="flex gap-3">
       <button
         type="button"
-        className={`py-3 px-5 rounded-md text-white w-[200px] ${className}`}
+        className={
+          "p-4 rounded-md text-white w-[200px] bg-sky-900 hover:bg-sky-700"
+        }
         onClick={() => {
           if (zoomIn) setZoomIn(undefined);
           else setZoomIn(id);

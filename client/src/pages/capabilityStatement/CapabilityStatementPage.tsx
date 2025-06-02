99d27187cefb09212b06d6107ffc27fd85466e37
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-02 04:42:46 +0200

    Use Card component in capability statement page

--- client/src/pages/capabilityStatement/CapabilityStatementPage.tsx
+++ client/src/pages/capabilityStatement/CapabilityStatementPage.tsx
@@ -1,6 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { createMetadataQueryOptions } from "../../query/metadata.query";
-import { Card } from "../../ui/Card";
 
 export function CapabilityStatementPage() {
   const { isPending, error, data } = useQuery(createMetadataQueryOptions());
@@ -16,44 +16,44 @@ export function CapabilityStatementPage() {
           return (
             <section key={i.mode} className="grid grid-cols-1 gap-2">
               {i.resource?.map((r) => (
-                <Card
-                  key={r.type}
-                  headerText={r.type}
-                  content={
-                    <>
+                <Card key={r.type}>
+                  <CardHeader>
+                    <CardTitle className="uppercase">{r.type}</CardTitle>
+                  </CardHeader>
+
+                  <CardContent>
+                    <section>
+                      <p className="font-bold">Interaction</p>
+                      <ul className="list-disc">
+                        {r.interaction?.map((i) => (
+                          <li key={i.code} className="ml-4">
+                            {i.code}
+                          </li>
+                        ))}
+                      </ul>
+                    </section>
+
+                    {!!r.searchParam?.length && (
                       <section>
-                        <p className="font-bold">Interaction</p>
+                        <p className="font-bold">Search Params</p>
                         <ul className="list-disc">
-                          {r.interaction?.map((i) => (
-                            <li key={i.code} className="ml-4">
-                              {i.code}
+                          {r.searchParam?.map((s) => (
+                            <li key={s.name} className="ml-4">
+                              <section className="flex flex-col">
+                                <span>
+                                  {s.name}&nbsp;-&nbsp;
+                                  {s.documentation?.toLocaleLowerCase()}
+                                </span>
+                                <span>{s.definition}</span>
+                                <span>type:&nbsp;{s.type}</span>
+                              </section>
                             </li>
                           ))}
                         </ul>
                       </section>
-
-                      {!!r.searchParam?.length && (
-                        <section>
-                          <p className="font-bold">Search Params</p>
-                          <ul className="list-disc">
-                            {r.searchParam?.map((s) => (
-                              <li key={s.name} className="ml-4">
-                                <section className="flex flex-col">
-                                  <span>
-                                    {s.name}&nbsp;-&nbsp;
-                                    {s.documentation?.toLocaleLowerCase()}
-                                  </span>
-                                  <span>{s.definition}</span>
-                                  <span>type:&nbsp;{s.type}</span>
-                                </section>
-                              </li>
-                            ))}
-                          </ul>
-                        </section>
-                      )}
-                    </>
-                  }
-                />
+                    )}
+                  </CardContent>
+                </Card>
               ))}
             </section>
           );

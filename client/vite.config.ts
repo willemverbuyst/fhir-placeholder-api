2a1885d3a1315f5f4caae1530de93efe07e30e79
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-02 03:42:36 +0200

    Set up shadcn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest/config" />
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",

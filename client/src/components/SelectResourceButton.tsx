635edf8e1cdaa0c5b3e7b79c3afc0e926327c525
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-02 04:55:01 +0200

    Style select resource button

--- client/src/components/SelectResourceButton.tsx
+++ client/src/components/SelectResourceButton.tsx
@@ -1,29 +1,24 @@
 import type React from "react";
 import { useSearchParams } from "react-router";
 import type { ConfigItems } from "../config/fhirResources";
-import { cn } from "../lib/utils";
+import { Button } from "./ui/button";
 
 export function SelectResourceButton({
   caption,
 }: {
   caption: keyof ConfigItems;
 }): React.JSX.Element {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const isSelected = searchParams.get("resource") === caption;
+  const [_, setSearchParams] = useSearchParams();
 
   return (
-    <button
+    <Button
       type="button"
-      className={cn(
-        "py-2 px-4 rounded-md text-white w-[350px] sm:w-[300px] text-center cursor-pointer font-bold transition-colors",
-        isSelected ? "bg-pink-500" : "bg-sky-900",
-        !isSelected && "hover:bg-sky-700",
-      )}
       onClick={() => {
         setSearchParams({ resource: caption });
       }}
+      variant="ghost"
     >
       {caption}
-    </button>
+    </Button>
   );
 }

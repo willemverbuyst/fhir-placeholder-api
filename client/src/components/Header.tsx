2194498d94a6faa37bd8ac3dad38ae7da92dccc7
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-02 04:28:55 +0200

    Re-style header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,33 +1,17 @@
 import { useState } from "react";
 import { NavLink } from "react-router";
 import { cn } from "../lib/utils";
-
-function MenuItem({ text, path }: { text: string; path: string }) {
-  return (
-    <NavLink
-      to={path}
-      className="flex h-full items-center p-4 text-zinc-200 font-bold aria-[current=page]:text-blue-300 hover:text-white hover:bg-white/10 transition-colors ease-in-out"
-    >
-      {text}
-    </NavLink>
-  );
-}
-
-function MobileMenuItem({ text, path }: { text: string; path: string }) {
-  return (
-    <NavLink
-      to={path}
-      className="relative flex h-full justify-center items-center p-4 text-zinc-200 font-bold aria-[current=page]:text-blue-300 hover:text-white hover:bg-white/10 transition-colors ease-in-out"
-    >
-      {text}
-    </NavLink>
-  );
-}
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
 
 export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <header className="w-full z-10 sticky flex items-center top-0 bg-gradient-to-r from-gray-900 to-gray-700 text-zinc-200 px-4">
+    <header className="w-full z-10 sticky flex items-center top-0 px-4">
       <h1 className="text-3xl p-2">Fhir Placeholder Api</h1>
       <div className="block md:hidden ml-auto my-auto cursor-pointer">
         <button
@@ -41,26 +25,63 @@ export function Header() {
             }
           }}
         >
-          <div className="bg-zinc-200 rounded-full w-8 h-1 group-open:rotate-45 transition-all top 0 group-open:top-2 relative" />
-          <div className="bg-zinc-200 rounded-full w-8 h-1 mt-1 opacity-100 group-open:opacity-0 transition-all" />
-          <div className="bg-zinc-200 rounded-full w-8 h-1 mt-1 group-open:-rotate-45 transition-all top-0 group-open:-top-2 relative" />
+          <div className="bg-black rounded-full w-8 h-1 group-open:rotate-45 transition-all top 0 group-open:top-2 relative" />
+          <div className="bg-black rounded-full w-8 h-1 mt-1 opacity-100 group-open:opacity-0 transition-all" />
+          <div className="bg-black rounded-full w-8 h-1 mt-1 group-open:-rotate-45 transition-all top-0 group-open:-top-2 relative" />
         </button>
-        <nav className="absolute top-[48px] left-0 bg-gradient-to-r from-gray-900 to-gray-700 w-full peer-open:block hidden">
-          <MobileMenuItem text="Home" path="/" />
-          <MobileMenuItem text="Graph" path="/graph" />
-          <MobileMenuItem
-            text="Search Sort Filter"
-            path="/search-sort-filter"
-          />
-          <MobileMenuItem text="Create" path="/create" />
-        </nav>
+        <section className="absolute top-[48px] left-0 w-full peer-open:flex hidden bg-white justify-center">
+          <NavigationMenu>
+            <NavigationMenuList className="flex flex-col bg-white w-full items-center">
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/">CapabilityStatement</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/graph">Graph</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/search-sort-filter">Search Sort Filter</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <NavLink to="/crate">Create</NavLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </section>
       </div>
-      <nav className="flex-1 md:flex hidden items-center justify-end">
-        <MenuItem text="Home" path="/" />
-        <MenuItem text="Graph" path="/graph" />
-        <MenuItem text="Search Sort Filter" path="/search-sort-filter" />
-        <MenuItem text="Create" path="/create" />
-      </nav>
+      <section className="flex-1 md:flex hidden items-center justify-end">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <NavLink to="/">CapabilityStatement</NavLink>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <NavLink to="/graph">Graph</NavLink>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <NavLink to="/search-sort-filter">Search Sort Filter</NavLink>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <NavLink to="/crate">Create</NavLink>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </section>
     </header>
   );
 }

24dadfec68d1c3c841aea2c3059f4713297dd15a
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-03 04:05:41 +0200

    Change layout of resource selectors

--- client/src/pages/create/CreatePage.tsx
+++ client/src/pages/create/CreatePage.tsx
@@ -29,7 +29,7 @@ export function CreatePage() {
 
   return (
     <div className="w-full min-h-[100vh] flex flex-col items-center gap-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+      <div className="flex">
         {APP_RESOURCE_TYPES.map((k) => (
           <Button
             key={k}

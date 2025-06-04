079874ebaa68393455476643f11bd7c10df68b6e
Willem Verbuyst <willemverbuyst@gmail.com>
2025-06-04 04:32:20 +0200

    Rearrange header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,10 +3,12 @@ import { Navigation } from "./Navigation";
 
 export function Header() {
   return (
-    <header className="w-full z-10 sticky flex items-center gap-10 top-0 px-4 bg-background">
-      <h1 className="text-3xl p-2">Fhir Placeholder Api</h1>
+    <header className="w-full z-10 sticky flex flex-col top-0 px-4 pb-4">
+      <section className="flex justify-between">
+        <h1 className="text-3xl p-2">Fhir Placeholder Api</h1>
+        <LightDarkModeToggle />
+      </section>
       <Navigation />
-      <LightDarkModeToggle />
     </header>
   );
 }

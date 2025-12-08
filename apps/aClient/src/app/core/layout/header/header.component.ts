9f326918ec2eaa6b1d27ce0a3fb5bfd2347039cc
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-12-08 10:32:33 +0100

    Work on header component

--- apps/aClient/src/app/core/layout/header/header.component.ts
+++ apps/aClient/src/app/core/layout/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: "app-header",
-  imports: [RouterLink],
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.scss",
+  selector: 'app-header',
+  imports: [RouterLink, MatToolbarModule, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {}

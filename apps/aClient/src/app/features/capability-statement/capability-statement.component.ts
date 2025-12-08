a429e5f65b55efb6bc2e03da4bf8b9f5a9f193eb
Willem Verbuyst <willemverbuyst@gmail.com>
2025-12-08 09:03:41 +0100

    Add mui

--- apps/aClient/src/app/features/capability-statement/capability-statement.component.ts
+++ apps/aClient/src/app/features/capability-statement/capability-statement.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from "@angular/material/card";
 import { CapabilityStatementRestResource } from "fhir/r5";
 import { MetadataService } from "../../core/services/capability-statement.service";
 
 @Component({
   selector: "app-capability-statement",
-  imports: [CommonModule],
+  imports: [CommonModule, MatCard, MatCardHeader, MatCardContent, MatCardTitle],
   templateUrl: "./capability-statement.component.html",
   styleUrl: "./capability-statement.component.scss",
 })

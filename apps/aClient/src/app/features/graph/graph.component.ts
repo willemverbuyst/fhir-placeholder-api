cce3349a8072d8e8dfb1ffdc1be2b136b3e2de43
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-12-12 09:23:03 +0100

    Clean up unused css

--- apps/aClient/src/app/features/graph/graph.component.ts
+++ apps/aClient/src/app/features/graph/graph.component.ts
@@ -1,12 +1,10 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { OrganizationComponent } from "./organization/organization.component";
+import { Component } from '@angular/core';
+import { OrganizationComponent } from './organization/organization.component';
 
 @Component({
-  selector: "app-graph",
-  imports: [MatButtonModule, CommonModule, OrganizationComponent],
-  templateUrl: "./graph.component.html",
-  styleUrl: "./graph.component.scss",
+  selector: 'app-graph',
+  imports: [OrganizationComponent],
+  templateUrl: './graph.component.html',
+  styleUrl: './graph.component.scss',
 })
 export class GraphComponent {}

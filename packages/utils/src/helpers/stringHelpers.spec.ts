622cdbdc524dede4e5d16f9d36fc36b788373a62
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-12-19 03:57:33 +0100

    Rename files and export

--- packages/utils/src/helpers/stringHelpers.spec.ts
+++ packages/utils/src/helpers/stringHelpers.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { isString, isTruthyString } from "./string";
+import { isString, isTruthyString } from "./stringHelpers";
 
 describe("isString", () => {
   it("should return true for string values", () => {

20014a2b08912e3d3ab37e4f545ebf9f3c238449
Willem Verbuyst <willemverbuyst@gmail.com>
2026-01-04 05:57:57 +0100

    Fix tests for allergy intolerance endpoint

--- apps/server/src/AllergyIntolerance/allergy-intolerance.controller.spec.ts
+++ apps/server/src/AllergyIntolerance/allergy-intolerance.controller.spec.ts
@@ -1,20 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AllergyIntoleranceController } from './allergy-intolerance.controller';
-import { AllergyIntoleranceService } from './allergy-intolerance.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AllergyIntoleranceController } from "./allergy-intolerance.controller";
+import { AllergyIntoleranceService } from "./allergy-intolerance.service";
 
-describe('AllergyIntoleranceController', () => {
+describe("AllergyIntoleranceController", () => {
   let controller: AllergyIntoleranceController;
+  let service: AllergyIntoleranceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AllergyIntoleranceController],
-      providers: [AllergyIntoleranceService],
+      providers: [
+        {
+          provide: AllergyIntoleranceService,
+          useValue: { findAll: jest.fn() },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AllergyIntoleranceController>(AllergyIntoleranceController);
+    controller = module.get<AllergyIntoleranceController>(
+      AllergyIntoleranceController,
+    );
+    service = module.get<AllergyIntoleranceService>(AllergyIntoleranceService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("findAll", () => {
+    it("should call findAll method of AppointmentService", () => {
+      controller.findAll();
+
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- apps/server/src/AllergyIntolerance/allergy-intolerance.service.spec.ts
+++ apps/server/src/AllergyIntolerance/allergy-intolerance.service.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AllergyIntoleranceService } from './allergy-intolerance.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { DataStoreService } from "../db/dataStore.service";
+import { AllergyIntoleranceService } from "./allergy-intolerance.service";
 
-describe('AllergyIntoleranceService', () => {
+describe("AllergyIntoleranceService", () => {
   let service: AllergyIntoleranceService;
+  const mockDataStore = {
+    allergies: [
+      {
+        id: "1",
+      },
+    ],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AllergyIntoleranceService],
+      providers: [
+        AllergyIntoleranceService,
+        { provide: DataStoreService, useValue: mockDataStore },
+      ],
     }).compile();
 
     service = module.get<AllergyIntoleranceService>(AllergyIntoleranceService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });

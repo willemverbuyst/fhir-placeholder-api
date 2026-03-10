f8e6949c2e543dbf7fee00625b1f61e06bf23748
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-10 16:08:42 +0100

    Set up tables app with nextjs

---
+++ apps/tables/app/patient/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function PatientPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-2xl font-bold">Patient</h1>
+        <section className="flex flex-col gap-4">
+          <Link href="/">Home</Link>
+        </section>
+      </main>
+    </div>
+  );
+}

517262dcec8056c4646993f981c4f17d8f466cb3
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-17 17:56:13 +0100

    feat: update FHIR terminology exports to include flag resources

---
+++ packages/dummy-data/src/resources/flag.ts
@@ -0,0 +1,48 @@
+import { faker } from "@faker-js/faker";
+import { FLAG_STATUS } from "@repo/fhir-terminology";
+import type { Flag } from "fhir/r5";
+import { IdGenerator } from "../idGenerator";
+
+export function generateFlag({
+  patientId,
+  encounterId,
+  id,
+}: {
+  patientId: string | undefined;
+  encounterId: string | undefined;
+  id: string;
+}): Flag {
+  return {
+    id,
+    resourceType: "Flag",
+    status: faker.helpers.arrayElement(FLAG_STATUS),
+    subject: { reference: patientId ? `Patient/${patientId}` : undefined },
+    encounter: {
+      reference: encounterId ? `Encounter/${encounterId}` : undefined,
+    },
+    code: { coding: [] },
+  };
+}
+
+export function generateFlags({
+  numberOfFlags,
+  numberOfPatients,
+  numberOfEncounters,
+  idGen,
+}: {
+  numberOfFlags: number;
+  numberOfPatients: number;
+  numberOfEncounters: number;
+  idGen: IdGenerator;
+}): Flag[] {
+  return Array.from({ length: numberOfFlags }, (_, i) => {
+    const patientIndex = Math.floor(i / (numberOfFlags / numberOfPatients));
+    const encounterIndex = Math.floor(i / (numberOfFlags / numberOfEncounters));
+
+    return generateFlag({
+      patientId: idGen.refs.get("patient")?.[patientIndex],
+      encounterId: idGen.refs.get("encounter")?.[encounterIndex],
+      id: idGen.generateId("flag"),
+    });
+  });
+}

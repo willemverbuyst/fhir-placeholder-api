8b8e322c0f32f6a0e7ec05e9f93bb3135ed56b0e
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-18 11:20:14 +0100

    feat: enhance HumanName struct and formatting logic for improved name handling

--- apps/fhir-name-service/main.go
+++ apps/fhir-name-service/main.go
@@ -5,11 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type HumanName struct {
-	Given  []string `json:"given"`
-	Family string   `json:"family"`
+	Family *string  `json:"family,omitempty" fhir:"cardinality=0..1"`
+	Given  []string `json:"given,omitempty" fhir:"cardinality=0..*"`
+}
+
+func formatHumanName(name HumanName) string {
+	given := strings.Join(name.Given, " ")
+	family := ""
+	if name.Family != nil {
+		family = *name.Family
+	}
+
+	if given == "" {
+		return family
+	}
+	if family == "" {
+		return given
+	}
+	return fmt.Sprintf("%s %s", given, family)
 }
 
 func formatName(w http.ResponseWriter, r *http.Request) {
@@ -18,19 +35,21 @@ func formatName(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var name HumanName
-	err := json.NewDecoder(r.Body).Decode(&name)
+	var names []HumanName
+	err := json.NewDecoder(r.Body).Decode(&names)
 	if err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 
-	if len(name.Given) == 0 {
-		http.Error(w, "Missing given name", http.StatusBadRequest)
-		return
+	var result string
+	if len(names) > 0 {
+		result = formatHumanName(names[0])
 	}
 
-	result := fmt.Sprintf("%s %s", name.Given[0], name.Family)
+	for i := 1; i < len(names); i++ {
+		result += ", " + formatHumanName(names[i])
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{

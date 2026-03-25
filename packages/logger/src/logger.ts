04b9023401067813bc2e1c8c05f8439e3f71a83a
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-25 09:13:07 +0100

    refactor: clean up logger

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,19 +1,10 @@
-import winston from "winston";
+import { createLogger, format, transports } from "winston";
 
-export const logger = ({
-  service,
-  level = "info",
-}: {
-  service: string;
-  level?: string;
-}) => {
-  return winston.createLogger({
-    level,
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json(),
-    ),
-    transports: [new winston.transports.Console()],
+export const logger = ({ service }: { service: string }) => {
+  return createLogger({
+    level: "info",
+    format: format.combine(format.timestamp(), format.json()),
+    transports: [new transports.Console()],
     defaultMeta: {
       service,
     },

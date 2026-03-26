370f795aa110cc490f621f231556a8f162396b3e
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-26 17:01:58 +0100

    chore: run python script before running app

--- overview/main.py
+++ overview/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import os
 
 
 def get_child_directory_names(directory_path: Path) -> list[str]:
@@ -9,8 +10,9 @@ def get_child_directory_names(directory_path: Path) -> list[str]:
 
 
 def main() -> None:
-  output_path = Path(__file__).resolve().parent / "overview.json"
-  project_root = output_path.parent.parent
+  default_output_path = Path(__file__).resolve().parent / "overview.json"
+  output_path = Path(os.getenv("OVERVIEW_OUTPUT_PATH", default_output_path))
+  project_root = Path(__file__).resolve().parent.parent
   overview_data = {
     "apps": get_child_directory_names(project_root / "apps"),
     "packages": get_child_directory_names(project_root / "packages"),

5c9095d7bbde28418c9866dc6eabca3de900394b
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-27 19:39:33 +0100

    chore: clean up

--- services/gateway/src/server.ts
+++ services/gateway/src/server.ts
@@ -12,8 +12,7 @@ const log = logger({ application: "gateway-service" });
 const authServiceUrl = process.env.AUTH_SERVICE_URL ?? "http://localhost:3001";
 const usersServiceUrl =
   process.env.USERS_SERVICE_URL ?? "http://localhost:3002";
-const fhirProxyTarget =
-  process.env.FHIR_PROXY_TARGET ?? "http://localhost:8080";
+const fhirProxyTarget = process.env.FHIR_SERVER_URL ?? "http://localhost:8080";
 
 const app = express();
 

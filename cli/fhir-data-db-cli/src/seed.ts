5aea859957d9652c8443afe7ce05a2f163d5a873
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-31 05:11:00 +0200

    chore: replace sqlite with postgres db

--- cli/fhir-data-db-cli/src/seed.ts
+++ cli/fhir-data-db-cli/src/seed.ts
@@ -1,5 +1,48 @@
-import { DB, seedDatabase } from "@repo/database-helpers";
+// import { DB, seedDatabase } from "@repo/database-helpers";
 
-const db = new DB("./fhir.db");
+import { pool } from "./db";
 
-seedDatabase(db);
+// const db = new DB("./fhir.db");
+
+// seedDatabase(db);
+
+async function createTable() {
+  console.log("Creating table...");
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      resource JSON
+    )
+  `);
+}
+
+async function cleanupTable() {
+  console.log("Cleaning up table...");
+  await pool.query(`
+    DELETE FROM users
+  `);
+}
+
+async function seedTable() {
+  console.log("Seeding table...");
+  const result = await pool.query(
+    `
+    INSERT INTO users (resource) VALUES ($1)
+  `,
+    [
+      JSON.stringify({
+        id: "1",
+        name: "John Doe",
+      }),
+    ],
+  );
+  console.log(result.rows);
+}
+
+async function main() {
+  await createTable();
+  await cleanupTable();
+  await seedTable();
+}
+
+main();

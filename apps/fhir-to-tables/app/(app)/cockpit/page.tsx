4cfcb6162aa0d4ab1dbb61191f4b2249b6c4e111
Willem Verbuyst <willemverbuyst@gmail.com>
2026-04-02 15:46:24 +0200

    chore: add pages

---
+++ apps/fhir-to-tables/app/(app)/cockpit/page.tsx
@@ -0,0 +1,3 @@
+export default function CockpitPage() {
+  return <h1 className="text-2xl font-bold text-center">Cockpit</h1>;
+}
---
+++ apps/fhir-to-tables/app/(app)/dendogram/page.tsx
@@ -0,0 +1,3 @@
+export default function DendogramPage() {
+  return <h1 className="text-2xl font-bold text-center">Dendogram</h1>;
+}

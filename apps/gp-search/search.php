55226ae5a54915bb4c607aa03ef9bfbe9770eb13
Willem Verbuyst <willemverbuyst@gmail.com>
2026-04-05 10:45:34 +0200

    feat: add search by organization

--- apps/gp-search/search.php
+++ apps/gp-search/search.php
@@ -166,8 +166,48 @@ function get_practitioner_by_phone() {
     return [];
 }
 
+function get_practitioner_by_organization() {
+    global $pdo, $query;
+
+    try {
+        $statement = $pdo->prepare(<<<'SQL'
+        SELECT p.resource, p.id
+        FROM organization o
+        JOIN practitioner_role pr
+        ON o.id = split_part(pr.resource #>> '{organization,reference}', '/', 2)::uuid
+        JOIN practitioner p
+        ON p.id = split_part(pr.resource #>> '{practitioner,reference}', '/', 2)::uuid
+        WHERE LOWER(o.resource->>'name') ILIKE :search;
+        SQL
+        );
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Failed to prepare organization search query']);
+        return [];
+    }
+
+    $searchPattern = '%'.$query.'%';
+
+    try {
+        $statement->execute([$searchPattern]);
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['error' => 'Failed to execute organization search query']);
+        return [];
+    }
+
+    $gps = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($gps) {
+        return $gps;
+    }
+
+    return [];
+}
+
 function get_organization_name_by_practitioner_id($practitioner_id) {
     global $pdo;
+
     try {
         $orgStatement = $pdo->prepare(<<<'SQL'
         SELECT o.resource  AS organization
@@ -182,7 +222,6 @@ function get_organization_name_by_practitioner_id($practitioner_id) {
         echo json_encode(['error' => 'Failed to prepare search query']);
         return null;
     }
-
     
     try {
         $orgStatement->execute(['id' => "Practitioner/".$practitioner_id]);
@@ -214,6 +253,10 @@ if ($criterion === 'email') {
 
 if ($criterion === 'phone') {
     $gps = get_practitioner_by_phone();
+}
+
+if ($criterion === 'organization') {
+    $gps = get_practitioner_by_organization();
 }
 
 $formatted_gps = array_map(function($gp) {

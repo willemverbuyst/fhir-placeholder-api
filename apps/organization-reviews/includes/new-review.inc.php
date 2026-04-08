83a34d5c99c18b1d99f877bf725993919f20f29a
Willem Verbuyst <willemverbuyst@gmail.com>
2026-04-08 09:00:12 +0200

    fix: include title in new review submission

--- apps/organization-reviews/includes/new-review.inc.php
+++ apps/organization-reviews/includes/new-review.inc.php
@@ -2,6 +2,7 @@
 session_start();
 
 if (isset($_POST['submit'])) {
+  $title = $_POST['title'];
   $organization = $_POST['organization'];
   $review = $_POST['review'];
   $author =$_SESSION['userid'];
@@ -10,7 +11,7 @@ if (isset($_POST['submit'])) {
   include "../classes/new-review.classes.php";
   include "../classes/new-review-contr.classes.php";
 
-  $newReview = new NewReviewContr($organization, $review, $author);
+  $newReview = new NewReviewContr($title,$organization, $review, $author);
   $newReview->submitReview();
 
   header("location: ../reviews.php");
